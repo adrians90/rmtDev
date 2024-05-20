e59e098673b85309582cb082714231ce38bf56a2
adrians90 <adriansandu9090@gmail.com>
2024-05-20 16:05:28 +0100

    Create useJobItem custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -12,25 +12,13 @@ import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
 import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
-import { useActiveId, useJobItems } from "../lib/hooks";
-import { BASE_API_URL } from "../lib/constants";
+import { useActiveId, useJobItem, useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const { jobItemsSliced: jobItems, isLoading } = useJobItems(searchText);
   const activeId = useActiveId();
-  const [jobItem, setJobItem] = useState(null);
-
-  useEffect(() => {
-    if (!activeId) return;
-    const fetchData = async () => {
-      const response = await fetch(`${BASE_API_URL}/${activeId}`);
-      const data = await response.json();
-      setJobItem(data.jobItem);
-    };
-
-    fetchData();
-  }, [activeId]);
+  const jobItem = useJobItem(activeId);
 
   return (
     <>
