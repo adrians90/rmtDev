bb6dd8fdaf42a2cd1876a0a1b1f03e07880b2a8d
adrians90 <adriansandu9090@gmail.com>
2024-05-20 17:10:21 +0100

    Move activeId and jobItem to JobItemContent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,13 +12,11 @@ import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
 import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
-import { useActiveId, useJobItem, useJobItems } from "../lib/hooks";
+import { useJobItems } from "../lib/hooks";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const { jobItemsSliced: jobItems, isLoading } = useJobItems(searchText);
-  const activeId = useActiveId();
-  const jobItem = useJobItem(activeId);
 
   return (
     <>
@@ -44,7 +42,7 @@ function App() {
 
           <PaginationControls />
         </Sidebar>
-        <JobItemContent jobItem={jobItem} />
+        <JobItemContent />
       </Container>
 
       <Footer />
