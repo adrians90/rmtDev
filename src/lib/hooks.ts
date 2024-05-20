e2869fd64dc81a861bd2970a48bfaaf7f926942b
adrians90 <adriansandu9090@gmail.com>
2024-05-20 13:13:42 +0100

    Type jobItems as JobItem[]

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
+import { JobItem } from "./types";
 
 export function useJobItems(searchText: string) {
-  const [jobItems, setJobItems] = useState([]);
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const jobItemsSliced = jobItems.slice(0, 7);
