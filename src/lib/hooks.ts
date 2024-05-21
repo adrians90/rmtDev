fa9b8c44204b859ea5e089ec1d4d4d27b56a7ad2
adrians90 <adriansandu9090@gmail.com>
2024-05-21 11:08:06 +0100

    Extract the debounce logic into a custom hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -65,3 +65,13 @@ export function useJobItems(searchText: string) {
 
   return { jobItemsSliced, isLoading, totalNumberOfResults } as const;
 }
+
+export function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timerId = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
