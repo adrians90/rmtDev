839d09e584b7c1f4502411caeb7e0db03f716613
adrians90 <adriansandu9090@gmail.com>
2024-05-22 11:50:03 +0100

    Install react-hot-toast for error rendering

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -47,7 +47,10 @@ const fetchJobItems = async (
   searchText: string
 ): Promise<JobItemsApiResponse> => {
   const response = await fetch(`${BASE_API_URL}?search=${searchText}`);
-
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
   const data = await response.json();
   return data;
 };
