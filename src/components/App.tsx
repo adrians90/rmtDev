9173b1263fb0b988465a93cb3e8559ae5200fcaa
adrians90 <adriansandu9090@gmail.com>
2024-05-22 12:31:01 +0100

    Implement pagination

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -20,9 +20,19 @@ function App() {
   const debouncedSearchText = useDebounce(searchText, 250);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
 
+  const [currentPage, setCurrentPage] = useState(1);
+
   const totalNumberOfResults = jobItems?.length || 0;
 
   const jobItemsSliced = jobItems?.slice(0, 7) || [];
+
+  const handleChangePage = (direction: "next" | "previous") => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "previous") {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
 
   return (
     <>
@@ -46,7 +56,10 @@ function App() {
 
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
 
-          <PaginationControls />
+          <PaginationControls
+            currentPage={currentPage}
+            onClick={handleChangePage}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
